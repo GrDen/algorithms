9bc17a9a43ffdcfd0ec045c6132cbd419a7c36bc
Denis Grischenko <grischenkodenis@gmail.com>
2022-05-07 00:24:48 +0300

    adds even-odd reorder problem

---
+++ code/arrays/even-odd.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> evenOddExtraSpace(vector<int>* v) {
+  vector<int>& vr = *v;
+  vector<int> even, odd, result;
+
+  for(int i=0; i < vr.size(); i++) {
+    if(vr[i] % 2 == 0) {
+      even.push_back(vr[i]);
+    } else {
+      odd.push_back(vr[i]);
+    }
+  }
+ 
+  for(int i=0; i < even.size(); i++) {
+    result.push_back(even[i]);
+  }
+
+  for(int i=0; i < odd.size(); i++) {
+    result.push_back(even[even.size() + i]);
+  }
+
+  return result;
+}
+
+
+void evenOddInPlace(vector<int>* v) {
+  vector<int>& vr = *v;
+  int evenNext = 0;
+  int oddNext = vr.size() - 1;
+
+  while(evenNext < oddNext) {
+    if(vr[evenNext] % 2 == 0) {
+      evenNext++;
+    } else {
+      int t = vr[evenNext];
+      vr[evenNext] = vr[oddNext];
+      vr[oddNext] = t;
+      oddNext--;
+    }
+  }
+}
+
+int main() {
+  vector<int> v = {1,2,3,4,5,6,7,8,9,11,12,16,21};
+
+  evenOddInPlace(&v);
+
+  for(int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+
+/*
+
+1 2 3 4 5 6 7
+
+
+
+*/
+
